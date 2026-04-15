7dc4336e6839ed0e0f0f684ab9e5fb0c74108b59
Benjamin Auquite <halomastar@gmail.com>
2026-04-15 08:19:54 -0500

    changes

--- Libraries/PyKotor/src/pykotor/kaitai_generated/gff_json.py
+++
@@ -1,5 +0,0 @@
-"""Shim: use ``bioware_kaitai_formats.gff_json`` directly."""
-
-from __future__ import annotations
-
-from bioware_kaitai_formats.gff_json import *  # noqa: F403
--- Libraries/PyKotor/src/pykotor/kaitai_generated/tlk_json.py
+++
@@ -1,5 +0,0 @@
-"""Shim: use ``bioware_kaitai_formats.tlk_json`` directly."""
-
-from __future__ import annotations
-
-from bioware_kaitai_formats.tlk_json import *  # noqa: F403
